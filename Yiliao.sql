6aa9943a2e9b2fe0a0e2a717f5d5c70154235bd4
sujiaqi <737273369@qq.com>
2022-11-20 00:40:55 +0800

    v2

--- Yiliao.sql
+++ Yiliao.sql
@@ -1,14 +1,51 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2022-11-18 17:25:29                          */
+/* Created on:     2022-11-20 0:37:16                           */
 /*==============================================================*/
 
 
+drop table if exists Carousel_chart;
+
+drop table if exists Large_components;
+
+drop table if exists Tips;
+
 drop table if exists User;
+
+drop table if exists Widgets;
 
 drop table if exists feedback;
 
 drop table if exists record;
+
+/*==============================================================*/
+/* Table: Carousel_chart                                        */
+/*==============================================================*/
+create table Carousel_chart
+(
+   CarId                int not null,
+   CarImage             longblob,
+   primary key (CarId)
+);
+
+/*==============================================================*/
+/* Table: Large_components                                      */
+/*==============================================================*/
+create table Large_components
+(
+   LcId                 int not null,
+   LcContent            varchar(10),
+   primary key (LcId)
+);
+
+/*==============================================================*/
+/* Table: Tips                                                  */
+/*==============================================================*/
+create table Tips
+(
+   Tip                  varchar(30) not null,
+   primary key (Tip)
+);
 
 /*==============================================================*/
 /* Table: User                                                  */
@@ -21,6 +58,18 @@ create table User
    Upassword            varchar(30),
    Uimage               longblob,
    primary key (UID)
+);
+
+/*==============================================================*/
+/* Table: Widgets                                               */
+/*==============================================================*/
+create table Widgets
+(
+   WId                  int not null,
+   LcId                 int not null,
+   Subtitle             varchar(20),
+   WContent             longtext,
+   primary key (WId)
 );
 
 /*==============================================================*/
@@ -50,6 +99,9 @@ create table record
    Proposal             varchar(60),
    primary key (CID)
 );
+
+alter table Widgets add constraint FK_Contain foreign key (LcId)
+      references Large_components (LcId) on delete restrict on update restrict;
 
 alter table feedback add constraint FK_RecordFeedback foreign key (CID)
       references record (CID) on delete restrict on update restrict;
