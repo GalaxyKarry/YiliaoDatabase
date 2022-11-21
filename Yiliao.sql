45031d285d038684e7933f3ee2cf7f9d3bbfa205
sujiaqi <737273369@qq.com>
2022-11-21 12:00:26 +0800

    添加应用场景

--- Yiliao.sql
+++ Yiliao.sql
@@ -1,8 +1,10 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2022-11-21 11:03:15                          */
+/* Created on:     2022-11-21 11:58:48                          */
 /*==============================================================*/
 
+
+drop table if exists Application;
 
 drop table if exists CarouselChart;
 
@@ -15,6 +17,18 @@ drop table if exists Record;
 drop table if exists User;
 
 drop table if exists Widgets;
+
+/*==============================================================*/
+/* Table: Application                                           */
+/*==============================================================*/
+create table Application
+(
+   AID                  int not null,
+   Hospital             varchar(30),
+   Floor                varchar(30),
+   Number               varchar(10),
+   primary key (AID)
+);
 
 /*==============================================================*/
 /* Table: CarouselChart                                         */
@@ -57,6 +71,7 @@ create table Record
 (
    CID                  int not null,
    UID                  int not null,
+   AID                  int,
    Pass                 int,
    Time                 char(30),
    Place                varchar(60),
@@ -98,6 +113,9 @@ alter table Feedback add constraint FK_RecordFeedback foreign key (CID)
 
 alter table Record add constraint FK_UserRecord foreign key (UID)
       references User (UID) on delete restrict on update restrict;
+
+alter table Record add constraint FK_address foreign key (AID)
+      references Application (AID) on delete restrict on update restrict;
 
 alter table Widgets add constraint FK_Contain foreign key (LcId)
       references LargeComponents (LcId) on delete restrict on update restrict;
